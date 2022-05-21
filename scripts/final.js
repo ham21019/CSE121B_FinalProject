689e7cc8238c0ec43d3911cf758fc57ff445e03c
ham21019 <ham21019@byui.edu>
2022-05-21 10:47:23 -0700

    add music

--- scripts/final.js
+++ scripts/final.js
@@ -9,9 +9,13 @@ Assignment: Final Project
 Research Sites:
     https://www.codecademy.com/resources/blog/javascript-projects-for-beginners/    
     https://www.interviewbit.com/blog/javascript-projects/
-    https://jsbeginners.com/javascript-projects-for-beginners/
+    https://jsbeginners.com/
     https://blog.boot.dev/javascript/javascript-projects-for-beginners/
 */
+
+// create variables
+const button = document.querySelector("#button");
+const audio = new Audio("https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3");
 
 // array of dadJokes
 const dadJokes = [
@@ -26,17 +30,18 @@ const dadJokes = [
     {
         joke: "Q: Why did the football coach go to the bank?\nA: To get his quarter back.",
         source: "https://www.thepioneerwoman.com/home-lifestyle/a35617884/best-dad-jokes/"
+    },
+    {
+        joke: "Q: What is Forrest Gump's password?\nA:1forrest1",
+        source: "https://www.thepioneerwoman.com/home-lifestyle/a35617884/best-dad-jokes/"
     }
 ];
 
-// create button variable
-const button = document.querySelector("#button");
-
-//perform action
+// perform action
 button.addEventListener("click", function () {
     let randomNumber = Math.floor(Math.random() * dadJokes.length);
     console.log(`The random number chosen was ${randomNumber}`);
     document.querySelector("#quote").textContent = dadJokes[randomNumber].joke;
     document.querySelector("#source").textContent = dadJokes[randomNumber].source;
+    audio.play();
 });
-
