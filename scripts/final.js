8fd306dd080683af9329cf5a8cc86624a0e99812
ham21019 <ham21019@byui.edu>
2022-05-21 09:58:42 -0700

    First coding

--- scripts/final.js
+++ scripts/final.js
@@ -4,7 +4,7 @@ Class: CSE121B
 Semester: Spring 2022
 Student: Mark Hammer
 email: ham21019@byui.edu
-Assignment: Week 06: Final Project
+Assignment: Final Project
 
 Research Sites:
     https://www.codecademy.com/resources/blog/javascript-projects-for-beginners/    
@@ -12,3 +12,31 @@ Research Sites:
     https://jsbeginners.com/javascript-projects-for-beginners/
     https://blog.boot.dev/javascript/javascript-projects-for-beginners/
 */
+
+// array of dadJokes
+const dadJokes = [
+    {
+        joke: "Q: How do you kill a blue elephant?\nA: With a blue elephant gun.",
+        source: "My dad"
+    },
+    {
+        joke: "Q: Why do elephants paint their toenails red?\nA: So they can hide in cherry trees.",
+        source: "My dad"
+    },
+    {
+        joke: "Q: Why did the football coach go to the bank?\nA: To get his quarter back.",
+        source: "https://www.thepioneerwoman.com/home-lifestyle/a35617884/best-dad-jokes/"
+    }
+];
+
+// create button variable
+const button = document.querySelector("#button");
+
+//perform action
+button.addEventListener("click", function () {
+    let randomNumber = Math.floor(Math.random() * dadJokes.length);
+    console.log(`The random number chosen was ${randomNumber}`);
+    document.querySelector("#quote").textContent = dadJokes[randomNumber].joke;
+    document.querySelector("#source").textContent = dadJokes[randomNumber].source;
+});
+
