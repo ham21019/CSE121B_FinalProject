83b948bcf084430ed492e4cf9fed7e4c99f71c02
ham21019 <ham21019@byui.edu>
2022-05-21 08:48:12 -0700

    Saturday Morning comments

---
+++ scripts/final.js
@@ -0,0 +1,14 @@
+/* 
+Instructor: Bro. Blazzard
+Class: CSE121B
+Semester: Spring 2022
+Student: Mark Hammer
+email: ham21019@byui.edu
+Assignment: Week 06: Final Project
+
+Research Sites:
+    https://www.codecademy.com/resources/blog/javascript-projects-for-beginners/    
+    https://www.interviewbit.com/blog/javascript-projects/
+    https://jsbeginners.com/javascript-projects-for-beginners/
+    https://blog.boot.dev/javascript/javascript-projects-for-beginners/
+*/
